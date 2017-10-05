ae711dd0bae2297a380bee9232dc705b1c513643
Kittyfisto <kittyfisto87@gmail.com>
2017-10-05 22:12:01 +0200

    PathComparer is public now
    Fixed several bugs, added tests => class is ready for consumption

---
+++ System.Extensions.Test/IO/PathComparerTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace System.Extensions.Test.IO
+{
+	[TestFixture]
+	public sealed class PathComparerTest
+	{
+		public static IEnumerable<string> InvalidPaths => new[]
+		{
+			null,
+			"",
+			"?"
+		};
+
+		[Test]
+		public void TestEquals1([ValueSource(nameof(InvalidPaths))] string path)
+		{
+			var comparer = new PathComparer();
+			comparer.Equals(path, "C:\\foo").Should().BeFalse();
+			comparer.Equals("C:\\foo", path).Should().BeFalse();
+		}
+
+		[Test]
+		public void TestEquals2()
+		{
+			var comparer = new PathComparer();
+			comparer.Equals("C:\\foo", "c:/foo").Should().BeTrue();
+		}
+
+		[Test]
+		public void TestGetHashCode1([ValueSource(nameof(InvalidPaths))] string path)
+		{
+			var comparer = new PathComparer();
+			new Action(() => comparer.GetHashCode(path)).ShouldNotThrow();
+			comparer.GetHashCode(path).Should().Be(comparer.GetHashCode(path));
+		}
+
+		[Test]
+		public void TestGetHashCode2()
+		{
+			var comparer = new PathComparer();
+			comparer.GetHashCode("C:\\foo").Should().Be(comparer.GetHashCode("c:/foo"));
+		}
+	}
+}
--- System.Extensions/IO/PathComparer.cs
+++ System.Extensions/IO/PathComparer.cs
@@ -8,9 +8,10 @@ namespace System.IO
 	///     Two strings are equal if they represent the same path, regardless
 	///     of formatting.
 	/// </summary>
-	internal sealed class PathComparer
+	public sealed class PathComparer
 		: IEqualityComparer<string>
 	{
+		/// <inheritdoc />
 		public bool Equals(string x, string y)
 		{
 			var nx = NormalizePath(x);
@@ -19,16 +20,30 @@ namespace System.IO
 			return string.Equals(nx, ny);
 		}
 
+		/// <inheritdoc />
 		public int GetHashCode(string obj)
 		{
 			var nobj = NormalizePath(obj);
-			return nobj.GetHashCode();
+			return nobj != null ? nobj.GetHashCode() : 0;
 		}
 
+		/// <summary>
+		///     Normalizes the given path so that differences in capitalisation
+		///     or used slashes don't matter anymore.
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
 		[Pure]
 		public static string NormalizePath(string path)
 		{
-			return Path.GetFullPath(new Uri(path).LocalPath)
+			if (string.IsNullOrEmpty(path))
+				return path;
+
+			Uri uri;
+			if (!Uri.TryCreate(path, UriKind.Absolute, out uri))
+				return path;
+
+			return Path.GetFullPath(uri.LocalPath)
 				.TrimEnd(Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar)
 				.ToUpperInvariant();
 		}
