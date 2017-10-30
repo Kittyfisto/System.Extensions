768b74220ce4d76f190631b52c79a92f8f440cba
Kittyfisto <kittyfisto87@gmail.com>
2017-10-31 00:29:44 +0100

    Introduced Stream.ReadToEnd() extension method

---
+++ System.Extensions.Test/IO/StreamExtensionsTest.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace System.Extensions.Test.IO
+{
+	[TestFixture]
+	public sealed class StreamExtensionsTest
+	{
+		[Test]
+		public void TestReadToEnd1()
+		{
+			new Action(() => ((Stream) null).ReadToEnd()).ShouldThrow<NullReferenceException>();
+		}
+
+		[Test]
+		public void TestReadToEnd2()
+		{
+			var stream = new MemoryStream();
+			stream.ReadToEnd().Should().BeEmpty();
+		}
+
+		[Test]
+		public void TestReadToEnd3()
+		{
+			var rng = new Random(42);
+			var data = new byte[4097];
+			rng.NextBytes(data);
+			var stream = new MemoryStream(data);
+			stream.ReadToEnd().Should().Equal(data);
+		}
+
+		[Test]
+		public void TestReadToEnd4()
+		{
+			var rng = new Random(42);
+			var data = new byte[4097];
+			rng.NextBytes(data);
+			var stream = new StreamProxy(new MemoryStream(data), allowSeek: false);
+			stream.ReadToEnd().Should().Equal(data);
+		}
+	}
+}
---
+++ System.Extensions/IO/StreamExtensions.cs
@@ -0,0 +1,53 @@
+﻿namespace System.IO
+{
+	/// <summary>
+	///     Provides extension methods for the stream class.
+	/// </summary>
+	public static class StreamExtensions
+	{
+		/// <summary>
+		///     Returns a new byte array which contains from this stream's <see cref="Stream.Position" />
+		///     until its end.
+		/// </summary>
+		/// <remarks>
+		///     This method will throw a <see cref="OutOfMemoryException" /> when no byte array of the required
+		///     size can be allocated.
+		/// </remarks>
+		/// <param name="that"></param>
+		/// <returns></returns>
+		public static byte[] ReadToEnd(this Stream that)
+		{
+			if (that == null)
+				throw new NullReferenceException();
+
+			using (var destination = CreateMemoryStream(that))
+			{
+				var buffer = new byte[4096];
+				int bytesRead;
+				while ((bytesRead = that.Read(buffer, offset: 0, count: buffer.Length)) > 0)
+					destination.Write(buffer, offset: 0, count: bytesRead);
+
+				return destination.ToArray();
+			}
+		}
+
+		private static MemoryStream CreateMemoryStream(Stream that)
+		{
+			if (that.CanSeek)
+			{
+				// We can find out the length of the buffer we need to allocate:
+				// This is preferrable because it allows us to skip the resizings
+				// MemoryStream would have to do...
+				var pos = that.Position;
+				var length = that.Length;
+				var remaining = length - pos;
+				var buffer = new byte[remaining];
+				return new MemoryStream(buffer);
+			}
+
+			// We cannot possibly find out the size of the stream without consuming it so
+			// resizing MemoryStream's internal buffer cannot be avoided.
+			return new MemoryStream();
+		}
+	}
+}
