de488efe98882345bff16bf00e055353b1f43967
Kittyfisto <kittyfisto87@gmail.com>
2017-10-31 01:44:16 +0100

    Added GZip class to simplify compressing
    GZipStream is a pain in the butt, hence this class....

---
+++ System.Extensions.Test/IO/GZipTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace System.Extensions.Test.IO
+{
+	[TestFixture]
+	public sealed class GZipTest
+	{
+		public static IEnumerable<byte[]> TestData => new[]
+		{
+			new byte[0],
+			new byte[] {1},
+			new byte[] {1, 2, 3, 4},
+			new byte[42],
+			new byte[1024],
+			Enumerable.Range(0, 1024 * 1024).Select(i => (byte)(i % byte.MaxValue)).ToArray()
+		};
+
+		public static IEnumerable<CompressionLevel> Levels => new[]
+		{
+			CompressionLevel.Optimal,
+			CompressionLevel.Fastest,
+			CompressionLevel.NoCompression
+		};
+
+		[Test]
+		public void TestRoundtrip1([ValueSource(nameof(TestData))] byte[] data)
+		{
+			var compressed = GZip.Compress(data);
+			var uncompressed = GZip.Decompress(compressed);
+			uncompressed.Should().Equal(data);
+
+			Console.WriteLine("Compressed {0} bytes to {1} bytes, down to {2:F1}% of original", data.Length, compressed.Length,
+				100.0*compressed.Length/data.Length);
+		}
+
+		[Test]
+		public void TestRoundtrip2([ValueSource(nameof(TestData))] byte[] data,
+			[ValueSource(nameof(Levels))] CompressionLevel level)
+		{
+			var compressed = GZip.Compress(data, level);
+			var uncompressed = GZip.Decompress(compressed);
+			uncompressed.Should().Equal(data);
+
+			Console.WriteLine("Compressed {0} bytes to {1} bytes, down to {2:F1}% of original", data.Length, compressed.Length,
+				100.0 * compressed.Length / data.Length);
+		}
+	}
+}
---
+++ System.Extensions/IO/GZip.cs
@@ -0,0 +1,75 @@
+﻿using System.IO.Compression;
+
+namespace System.IO
+{
+	/// <summary>
+	///     Provides methods to compress/decompress byte array (using the <see cref="GZipStream" />).
+	/// </summary>
+	public static class GZip
+	{
+		/// <summary>
+		///     The size of the buffer used to perform block-copies.
+		/// </summary>
+		/// <remarks>
+		///     It makes sense to align this with the typical page size, no?.
+		/// </remarks>
+		private const int BufferSize = 4096;
+
+		/// <summary>
+		///     Compressses the given byte array using the GZip algorithm
+		///     and <see cref="CompressionLevel.Optimal" />.
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static byte[] Compress(byte[] data)
+		{
+			return Compress(data, CompressionLevel.Optimal);
+		}
+
+		/// <summary>
+		///     Compressses the given byte array using the GZip algorithm
+		///     and the <paramref name="compressionLevel" />.
+		/// </summary>
+		/// <param name="data"></param>
+		/// <param name="compressionLevel"></param>
+		/// <returns></returns>
+		public static byte[] Compress(byte[] data, CompressionLevel compressionLevel)
+		{
+			using (var uncompressed = new MemoryStream(data))
+			{
+				using (var compressed = new MemoryStream())
+				{
+					using (var gzip = new GZipStream(compressed, compressionLevel))
+					{
+						uncompressed.CopyTo(gzip);
+					}
+
+					return compressed.ToArray();
+				}
+			}
+		}
+
+		/// <summary>
+		///     Decompresses the given data which has been previously compressed using the GZip algorithm
+		///     (either via <see cref="Compress(byte[])" />, <see cref="Compress(byte[], CompressionLevel)" />,
+		///     <see cref="GZipStream" /> or any equivalent method).
+		/// </summary>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		public static byte[] Decompress(byte[] data)
+		{
+			using (var compressedStream = new MemoryStream(data))
+			{
+				using (var decompressed = new MemoryStream())
+				{
+					using (var gzip = new GZipStream(compressedStream, CompressionMode.Decompress))
+					{
+						gzip.CopyTo(decompressed);
+					}
+
+					return decompressed.ToArray();
+				}
+			}
+		}
+	}
+}
