df0ea9f488223b1d581d3554c8eb16115b60e0a8
Simon Mießler <simon.miessler@gmx.net>
2021-11-13 00:33:10 +0100

    Fixing the fix

--- publish.cmd
+++ publish.cmd
@@ -6,8 +6,9 @@ echo Files in directory:
 dir . /a-d
 
 echo Looking for nuget package...
-FOR %%F IN (Bin/Release/System.Threading.Extensions.*.nupkg) DO (
- set NUGET_PACKAGE=%%F
+set FOLDER=Bin/Release
+FOR %%F IN (%FOLDER%/System.Threading.Extensions.*.nupkg) DO (
+ set NUGET_PACKAGE=%FOLDER%/%%F
 )
 
 if not "%NUGET_PACKAGE%"=="" (
@@ -16,7 +17,7 @@ if not "%NUGET_PACKAGE%"=="" (
 		echo Publishing nuget package...
 		@nuget push -Source https://api.nuget.org/v3/index.json -ApiKey %NUGET_API_KEY% %NUGET_PACKAGE%
 	) else (
-		echo Not publishing from this branch...
+		echo Not publishing from branch "%APPVEYOR_REPO_BRANCH%"...
 	)
 ) else (
 	echo Did not find anything to publish!
