f4c348d594e5b6eecc09d4631d11a5066c9e7a2b
Simon Mießler <simon.miessler@gmx.net>
2021-11-12 22:19:47 +0100

    Test

--- restore_key.cmd
+++ restore_key.cmd
@@ -4,3 +4,5 @@
 sig\crypt.exe decrypt sig\sns key.snk
 sig\crypt.exe enablesigning src\System.Extensions\System.Extensions.csproj ..\..\key.snk
 sig\crypt.exe enablesigning src\System.Extensions.Test\System.Extensions.Test.csproj ..\..\key.snk
+
+more src\System.Extensions\System.Extensions.csproj
